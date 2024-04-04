d116cb48216d1470f61f95ec6bada5c4e3261b43
Ethan <85865562+ethanlew2008@users.noreply.github.com>
2024-04-04 21:56:19 +0100

    Update and rename Compare powers to Comparepowers.cs

--- 6kyu/Comparepowers.cs
+++ 6kyu/Comparepowers.cs
@@ -5,13 +5,9 @@ class Kata
     {
       double one = a[1] * Math.Log(a[0]);
       double two = b[1] * Math.Log(b[0]);
-      
-      
-      
-      
+        
       if(one>two){return -1;}
       if(one == two){return 0;}
-      else{return 1;}
-      
+      else{return 1;} 
     }
 }
